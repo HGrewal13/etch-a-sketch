f99afda6283ffaf2ceb86fe12d503b1e94bd0cc5
Harry Grewal <harrysgrewal13@gmail.com>
2025-04-10 16:32:10 -0400

    square width and height is now calculated and then dynamically created to ensure creating within container

--- scripts.js
+++ scripts.js
@@ -1,16 +1,19 @@
-let container = document.querySelector("#container");
+let sketchContainer = document.querySelector("#sketchContainer");
 let clearButton = document.querySelector("#clearBoard");
 
 function createGrid(size) {
+    let sketchContainerWidth = sketchContainer.offsetWidth;
 	for(let i = 0; i < size; i++) {
   	let row = document.createElement("div");
     row.classList.add("row");
     for(let j = 0; j < size; j++) {
-    	let column = document.createElement("div");
-      column.classList.add("createdDiv");
-      row.appendChild(column);
+        let column = document.createElement("div");
+        column.classList.add("createdDiv");
+        column.style.width = `calc(${sketchContainerWidth}px/${size})`;
+        column.style.height = `calc(${sketchContainerWidth}px/${size})`;
+        row.appendChild(column);
     }
-    container.appendChild(row);
+    sketchContainer.appendChild(row);
   }
 }
 
@@ -27,14 +30,14 @@ function resetBoard() {
 };
 
 function resetBoard() {
-	let squares = container.querySelectorAll(".createdDiv");
+	let squares = sketchContainer.querySelectorAll(".createdDiv");
   squares.forEach((square) => square.style.backgroundColor = "white");
 };
 
 let gridDivs = document.querySelectorAll(".createdDiv");
 /* gridDivs.addEventListener("click", highlight); */
 
-container.addEventListener("mouseover", function(event) {
+sketchContainer.addEventListener("mouseover", function(event) {
   if(event.target.className == "createdDiv") {
   	event.target.style.backgroundColor = "blue";
   }
